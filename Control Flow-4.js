fb0d770ec2baf0093c51a649c23c3e0fe0ca1f6f
Camila Omenna <112710301+CamilaOmenna@users.noreply.github.com>
2022-09-29 13:53:34 -0300

    Add files via upload

---
+++ Control Flow-4.js
@@ -0,0 +1,15 @@
+const prompt = require('prompt sing')();
+
+let número = parseInt(prompt('O numero  '));
+
+if(numero % 4== 0) {
+
+    numero = Math.sqrt(numero,8)
+     console.log('Par')
+     console.log(`${numero}`)
+} else {
+    
+    number = Math.pow(numero,9)
+    console.log('Impar')
+    console.log(`${numero}`)
+}
